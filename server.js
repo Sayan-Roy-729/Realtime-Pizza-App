550bfc1ca3f7f8c44b7ce81af03515b85de76d22
Sayan Roy <rsayan553@gmail.com>
2021-06-04 22:43:33 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+
+// set Template engine
+app.use(express.static(path.join(__dirname, 'resources')));
+app.set('views', path.join(__dirname, 'resources', 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res, next) => {
+    res.render('home');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
