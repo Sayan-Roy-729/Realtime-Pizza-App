add9976a720f468eb982bc8ffe8bd7d913fc7a42
Sayan Roy <rsayan553@gmail.com>
2021-06-05 11:57:41 +0530

    add cart page, auth

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const app = express();
 
 // set Template engine
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayout);
 app.set('views', path.join(__dirname, 'resources', 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res, next) => {
     res.render('home');
+});
+
+app.get('/cart', (req, res, next) => {
+    res.render('customers/cart');
+});
+
+app.get('/login', (req, res, next) => {
+    res.render('auth/login');
+});
+
+app.get('/register', (req, res, next) => {
+    res.render('auth/register');
 });
 
 const PORT = process.env.PORT || 3000;
